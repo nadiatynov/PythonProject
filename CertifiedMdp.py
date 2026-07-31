f81099b0081c67799423ba81244193a8c94a9612
knadiath <nadiath.karimou@ynov.com>
2026-07-31 18:52:45 +0200

    Base with upper and lower letters, digit and char special

---
+++ CertifiedMdp.py
@@ -0,0 +1,32 @@
+#len(mdp) >= 12
+#En python les import doivent être rangés deans l'ordre alphabétique(on regarde la 1ère lettre de l'import)
+
+import random
+import string
+
+choice = input("Quelle longueur voulez-vous pour votre mot de passe: ")
+
+mdp = []
+
+#string.ascii_uppercase renvoie une chaine de caractères avec toutes les lettres de l'alphabet en majuscule
+#string.ascii_lowercase fait pareil à la diff que les lettres sont en miniscule
+#string.digits renvoie une chaine de caractère contenant les chiffres de 0 à 9
+#string.punctuation renvoie une chaine de caractères avec tous les caractères spéciaux 
+#random.choice() permet de choisir aléatoirement un élément dans une liste 
+
+Maj = random.choice(string.ascii_uppercase)
+Min = random.choice(string.ascii_lowercase)
+number = random.choice(string.digits)
+sp = random.choice(string.punctuation)
+
+mdp.extend([Maj,Min,number,sp])
+
+random.shuffle(mdp) #random.shuffle() permet de mélanger une liste sur place de façon aléatoire
+
+result = ''.join(mdp)
+
+choice = input("Quelle longueur voulez-vous pour votre mot de passe: ")
+
+print("--- Générateur de Mot de Passe Certifié ---")
+
+print(f"Votre mot de passe sécurisé: {result}")
